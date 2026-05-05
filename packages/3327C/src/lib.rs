5c311ec24b7b7f00ee00fcd1fcc2901b72331e23
Russel Einsla <speedskater1610@gmail.com>
2026-05-05 12:23:37 -0400

    Create lib.rs with module declarations
    Initial module declarations for the 3327C library.

---
+++ packages/3327C/src/lib.rs
@@ -0,0 +1,12 @@
+//! # 3327C/src/lib.rs
+//! 
+//! Lib for our controls
+//!
+//! ## Usage
+
+pub mod theme;
+pub mod subsystems;
+pub mod logger;
+pub mod hardware;
+pub mod motion;
+pub mod image;
